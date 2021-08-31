83b03a232b37641e37afd52625f3957704504f5a
Anilin <anilinr@gmail.com>
2021-08-31 19:15:49 +0200

    updates trying to get deployment right

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React WEH
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
